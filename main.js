a14c7844ea7d3ed39524c87286d70c63537c44f4
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-16 10:51:57 +0300

    add more action

--- main.js
+++ main.js
@@ -2,5 +2,19 @@
 //key-value pairs in curly braces
 
 const myObj = {name: "Dave"}
+// console.log(myObj.name);
 
-console.log(myObj.name);
+const anotherObj = {
+    alive: true,
+    answer: 42,
+    hobbies: ["Eat","Sleep","Code"],
+    beverage: {
+        morning: "Coffee",
+        afternoon: "Iced Tea"
+    },
+    action: function(){
+        return `Time for ${this.beverage.morning}`;
+    }
+}
+
+console.log(anotherObj.action())
